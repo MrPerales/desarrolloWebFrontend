5e1970971e998609bfa68a9fcb62f41d090b4779
Carlos Perales <carlosperales050@gmail.com>
2022-12-25 20:40:54 -0600

    Se agrega mobile menu and the  aside carrito de compras

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,42 @@
 const menuEmail=document.querySelector(".navbar-Email");
 const desktopMenu=document.querySelector(".menu-desktop");
+const burgerMenu=document.querySelector(".burgerMenu");
+const mobileMenu=document.querySelector(".mobile-menu");
+const menuCarritoIcon=document.querySelector(".navbar-carrito-icon");
+const aside=document.querySelector(".product-datail")
 
-menuEmail.addEventListener("click",toggleDesktopMenu);
+menuEmail.addEventListener("click" ,toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click",toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isAsideClosed=aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+
     desktopMenu.classList.toggle("inactive");
+
 }
 
+function toggleMobileMenu(){
+    const isAsideClosed=aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
+}
+function toggleCarritoAside(){
+
+    const isMobileMenuClosed=mobileMenu.classList.contains("inactive");
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive");
+    }
+    aside.classList.toggle("inactive");
+}
 
